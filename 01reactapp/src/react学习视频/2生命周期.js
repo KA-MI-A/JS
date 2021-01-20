1b1cb9035583c88a2464cf1180dd23760f71b6ea
xjl <1944559669@qq.com>
2021-01-20 20:43:38 +0800

    flx：案例

--- 01reactapp/src/react学习视频/2生命周期.js
+++ 01reactapp/src/react学习视频/2生命周期.js
@@ -1,57 +1,92 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Clock extends React.Component{
-    constructor(props){
-        super(props)
-        //状态（数据）--》view
-        //构造函数初始化数据，将需要改变的数据初始化到state中
-        this.state = {
-            time:new Date().toLocaleTimeString()
-        }
-        //console.log(this.state.time)
-    }
 
+
+class ComLife extends Component{
+    constructor(props){
+        super(props)//调用继承Component的构造函数
+        this.state = {
+            msg:"hello world"
+        }
+        console.log("constructor构造函数")
+
+    }
+    componentWillMount(){
+        console.log("componentWillMount组件将要渲染")
+    }
+    componentDidMount(){
+        console.log("componentDidMount组件渲染完毕")
+    }
+    componentWillReceiveProps(){
+        console.log("componentWillReceiveProps组件将要接收新的state和props")
+    }
+    shouldComponentUpdate(){
+        //如果希望更新，就返回为真，不希望更新就返回为false
+        console.log("进行判断是否要更新内容")
+        console.log(this.state.msg)
+        if(this.state.msg==="hello world"){
+            console.log(true)
+            return true
+        }else{
+            console.log(false)
+            return false
+        }
+        
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate组件将要更新')
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate组件更新完毕')
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount移除')
+    }
     render(){
-        //console.log("这是渲染函数")
-        //this.state.time = new Date().toLocaleTimeString();
+        console.log('render渲染函数')
         return (
             <div>
-                <h1>当前时间：{this.state.time}</h1>
+                <h1>{this.state.msg}</h1>
+                <button onClick={this.changeMsg}>组件更新</button>
             </div>
         )
     }
-
-    //生命周期函数,组件渲染完成时的函数
-    componentDidMount(){
-        setInterval(()=>{
-            console.log(this.state.time)
-            //this.state.time = new Date().toLocaleTimeString(); //错误的方式
-            //切勿直接修改state数据，直接state重新渲染内容，需要使用setState
-            //通过this.setState修改完数据后，并不会立即修改DOM里面的内容,react会在，
-            //这个函数内容所有设置状态改变后，统一对比虚拟DOM对象，然后在统一修改，提升性能。
-            //小程序也是也是借鉴REACT状态管理操作
-            this.setState({
-                time:new Date().toLocaleTimeString()
-            })
-            
-        },1000)
+    changeMsg=()=>{
+        this.setState({
+            msg:"hello 老陈"
+        })
     }
-
 }
 
-
+class ParentCom extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            isShow:true
+        }
+    }
+    render(){
+        if(this.state.isShow){
+            return (
+                <div>
+                    <button onClick={this.removeCom}>移除comlife</button>
+                    <ComLife></ComLife>
+                </div>
+            )
+        }else{
+            return <h1>将comlife已移除</h1>
+        }
+    }
+    removeCom=()=>{
+        this.setState({
+            isShow:false
+        })
+    }
+}
 
 ReactDOM.render(
-    <Clock />,
+    <ParentCom></ParentCom>,
     document.querySelector('#root')
 )
-
-
-
-
-
-
-
-
